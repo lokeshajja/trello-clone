e647a2a094c745e174a13fb69de8d4ddd6e096b8
Lokesh S/A <lokesh.ajja@bulkmro.tech>
2020-06-03 21:30:24 +0530

    postgres database is configured

--- trelco/trelco/settings.py
+++ trelco/trelco/settings.py
@@ -76,32 +76,32 @@ WSGI_APPLICATION = 'trelco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-
 #     'default': {
-
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-#         'NAME': ‘<db_name>’,
-
-#         'USER': '<db_username>',
-
-#         'PASSWORD': '<password>',
-
-#         'HOST': '<db_hostname_or_ip>',
-
-#         'PORT': '<db_port>',
-
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-
 # }
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'trelco',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'loki',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
