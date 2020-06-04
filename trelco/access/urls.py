f90aca1b14c9373c29884837bc5ca88b156890d5
Lokesh S/A <lokesh.ajja@bulkmro.tech>
2020-06-04 14:22:06 +0530

    urls added

--- trelco/access/urls.py
+++ trelco/access/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from access.views.board_view import BoardListApi, BoardDetailView, index
+from access.views.list_view import ListListApi, ListDetailView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('boards', BoardListApi.as_view(), name='boards_listing_view'),
+    path('boards/<board_id>', BoardDetailView.as_view(), name = 'boards_detail_view'),
+    path('lists', ListListApi.as_view(), name='lists_listing_view'),
+    path('lists/<list_id>', ListDetailView.as_view(), name = 'lists_detail_view'),
+
+
     
 ]
